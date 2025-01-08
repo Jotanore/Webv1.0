b803679cf39e2414ff063137cab7830af6b4b1b2
Julen Garcia <144489575+Jotanore@users.noreply.github.com>
2025-01-08 17:17:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const project = document.querySelector('.slide');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const getRepo = async function () {
+  const profile = await fetch('https://api.github.com/users/Jotanore');
+  const repo = await fetch('https://api.github.com/users/Jotanore/repos');
+  const profilejson = await profile.json();
+  const repojson = await repo.json();
+  repojson.forEach(function (a) {
+    renderCard(a);
+  });
+  console.log(profilejson);
+  console.log(repojson);
+};
+getRepo();
+
+const renderCard = function (n) {
+  const html = `
+    <div class="card">
+        <div>
+        <a href="${n.svn_url}">
+          <img src="img/hero.png" class="card__image" alt="">
+          </a>
+        </div>
+        <div class="card__text"><h4>${n.name}</h4>
+        </div>
+      </div>`;
+  project.insertAdjacentHTML('afterbegin', html);
+};
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
